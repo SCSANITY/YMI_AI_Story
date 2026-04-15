6efb2ff4fbbc462536a2ded85575d698980a11cb
unknown <ymistory1@gmail.com>
2026-04-15 14:25:40 +0800

    Tweak banner spacing and mobile book cards

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         height={2000}
         aspectClassName="aspect-[3/1]"
         hotspotClassName="left-[39.8%] top-[65.8%] h-[15.8%] w-[22.8%]"
-        className="-mt-1 md:-mt-2"
+        className="-mt-2 md:-mt-3"
       />
       <BookList />
       <HomePosterBanner
